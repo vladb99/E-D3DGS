9e805351ff99254dd8a5cdc6f4524babe7d220d2
vladb99 <vladut.bratulescu@gmail.com>
2025-01-30 17:38:32 +0100

    Add statistical filtering in mesh extraction

--- utils/mesh_extraction_utils.py
+++ utils/mesh_extraction_utils.py
@@ -2,6 +2,9 @@ from scene import Scene
 import torch
 import trimesh
 from utils.general_utils import build_rotation
+from utils.extra_utils import o3d_knn
+import open3d as o3d
+import numpy as np
 
 @torch.no_grad()
 def get_tetra_points(rotation_not_activated, xyz, scale_after_3D_filter):
@@ -17,6 +20,32 @@ def get_tetra_points(rotation_not_activated, xyz, scale_after_3D_filter):
     # xyz = xyz[mask]
     # scale = scale[mask]
     # rots = rots[mask]
+
+    # neighbors_sq_distances, _ = o3d_knn(xyz.detach().cpu().numpy(), 10)
+    # neighbors_mean_distance = torch.from_numpy(neighbors_sq_distances.mean(axis=1))
+    # _, indices = torch.topk(neighbors_mean_distance, k=int(neighbors_mean_distance.shape[0] * 0.05))
+    # mask = torch.ones(xyz.shape[0], device=xyz.device)
+    # mask = torch.round(mask).bool().squeeze()
+    # mask[indices] = 0
+
+    # pcd = o3d.geometry.PointCloud()
+    # pcd.points = o3d.utility.Vector3dVector(xyz.detach().cpu().numpy())
+    # _, indices = pcd.remove_radius_outlier(nb_points=80, radius=0.02)
+    # print(np.asarray(indices).shape)
+    # mask = torch.zeros(xyz.shape[0], device=xyz.device)
+    # mask = torch.round(mask).bool().squeeze()
+    # mask[indices] = 1
+
+    pcd = o3d.geometry.PointCloud()
+    pcd.points = o3d.utility.Vector3dVector(xyz.detach().cpu().numpy())
+    _, indices = pcd.remove_statistical_outlier(nb_neighbors=20, std_ratio=1.0)
+    mask = torch.zeros(xyz.shape[0], device=xyz.device)
+    mask = torch.round(mask).bool().squeeze()
+    mask[indices] = 1
+
+    xyz = xyz[mask]
+    scale = scale[mask]
+    rots = rots[mask]
 
     vertices = M.vertices.T
     vertices = torch.from_numpy(vertices).float().cuda().unsqueeze(0).repeat(xyz.shape[0], 1, 1)
